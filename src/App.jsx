8b90f69fb415fa1a5299072b18f999a49c26cbe4
5424076ktbya <chya24076@g.nihon-u.ac.jp>
2026-01-22 02:44:31 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+if (typeof window !== "undefined") {
+  const meta = document.createElement('meta');
+  meta.name = "viewport";
+  meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no";
+  document.getElementsByTagName('head')[0].appendChild(meta);
+}
+
 import { useState, useEffect } from "react";
 import { runSlumpSimulation } from "./simulator";
 import ResultChart from "./ResultChart";
